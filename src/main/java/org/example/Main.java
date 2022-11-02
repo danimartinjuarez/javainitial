659a9ac76e091f1ed01a79ac93c2681f09d010a1
dani martin <danimartinjuarez@gmail.com>
2022-11-02 16:04:43 +0100

    inicial java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        byte byteVariable= 8;
+        short shortVariable= 88;
+        int intVariable= 16;
+        long longVariable= 32;
+        float floatVariable= 64.12f;
+        double doubleVariable= 1.02d;
+        boolean booleanVariable= false;
+        boolean booleanVariable2= true;
+        char charVariable= 'a';
+        String stringVariable= "Hola mundo";
+        System.out.println("En java nos podemos encontrar con variables numericas de tipo byte "+ byteVariable+" de tipo short "+ shortVariable+" de tipo integer "+ intVariable+" de tipo long "+ longVariable +" de tipo float "+ floatVariable+" de tipo double " + doubleVariable+ " de tipo float " + floatVariable + " de tipo boleano(verdadro o falso) " +booleanVariable +" or "+ booleanVariable2 +" de tipo char " +charVariable+ " ,o de tipo string "+stringVariable );
+    }
+}
